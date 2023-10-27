efe941d72dfe073c3ce9907cf6f2b0faec1c93d8
kosa355 <251153@edu.p.lodz.pl>
2023-10-27 09:15:45 +0200

    01 zmiana metody Policz

--- main.c
+++ main.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
+int Policz(int a, int b){
+    return a+b;
+}
 int main() {
     printf("Hello, World!\n");
+    printf("W pierwszym branchu policz: %d", Policz(2, 3));
     return 0;
 }
